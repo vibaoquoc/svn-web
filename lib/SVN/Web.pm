f756019b3892113aec114df25963f7d08831a6ef
nik <nik@714b6732-7f46-11dd-bf14-85d1fe059cc0>
2005-10-28 15:26:41 +0000

    Add detailed documentation about configuring SVN::Web via config.yaml.
    For consistency, rename @PLUGINS to @DEFAULT_ACTIONS.  This list can now
    be overridden within config.yaml, and the action passed to the CGI script
    is checked to make sure that it's allowed.
    
    This makes it possible to create a restricted SVN::Web installation -- for
    example, the 'checkout' action could be removed from the configuration file.
    This also makes it easier to add new actions to the list.
    
    Add a "meta-filter" for log messages.  This is a Template::Toolkit filter
    that can be configured to run multiple filters from a single filter
    directive in the Template.  If the meta-filter is configured to run any
    filters that don't exist on the system then they are silently skipped.
    
    This means that the filtering of log messages can be controlled from the
    config file, rather than requiring a change to every template that displays
    a log message.  It also means that we can ship with several useful filters
    turned on (eg, ::Clickable, and Clickable::Email) and still work if the
    user hasn't installed those filters.
    
    Adjust the templates that display log messages to use this new filter.  
    Change the config file installed by svnweb-install to specify a sensible
    default set.
    
    There's quite a lot of my work gone in here, so add myself to the AUTHORS
    and COPYRIGHT sections.

--- lib/SVN/Web.pm
+++ lib/SVN/Web.pm
@@ -48,6 +48,280 @@ diff.
 SVN::Web also tracks the branching feature (node copy) of subversion,
 so you can easily see the relationship between branches.
 
+
+=head1 CONFIGURATION
+
+Various aspects of SVN::Web's behaviour can be controlled through the
+configuration file F<config.yaml>.  See the C<YAML> documentation for
+information about writing YAML format files.
+
+=head2 Repositories
+
+SVN::Web can show information from one or more Subversion repositories.
+
+To specify them use C<repos> or C<reposparent>.
+
+If you have a single Subversion repository, or multiple repositories that
+are not under a single parent directory then use C<repos>.
+
+  repos:
+    first_repo: '/path/to/the/first/repo'
+    second_repo: '/path/to/the/second/repo'
+
+If you have multiple repositories that are all under a single parent
+directory then use C<reposparent>.
+
+  reposparent: '/path/to/parent/directory'
+
+If you set C<reposparent> then you can selectively block certain repositories
+from being browseable by specifying the C<block> setting.
+
+  block:
+    - 'first_subdir_to_block'
+    - 'second_subdir_to_block'
+
+=head2 Templates
+
+SVN::Web's output is entirely template driven.  SVN::Web ships with a
+number of different template styles, installed in to the F<templates/>
+subdirectory of wherever you ran C<svnweb-install>.
+
+The default templates are installed in F<templates/default>.  To change
+to another set, use the C<templatedir> configuration directive.
+
+For example, to use a set of templates that implement a look and feel similar
+to the Trac (L<http://www.edgewall.com/trac/>) output;
+
+  templatedir: 'template/trac'
+
+Alternatively, if you have your own templates elsewhere you can
+specify a full path to the templates.
+
+  templatedir: '/full/path/to/template/directory'
+
+For more information about writing your own templates see
+L</"ACTIONS, SUBCLASSES, AND URLS">.
+
+=head2 Log message filters
+
+Many of the templates shipped with SVN::Web include log messages from
+the repository.  It's likely that these log messages contain e-mail
+addresses, links to other web sites, and other rich information.
+
+The Template::Toolkit makes it possible to filter these messages through
+one or more plugins and/or filters that can recognise these and insert
+additional markup to make them active.
+
+There are two drawbacks with this approach:
+
+=over 4
+
+=item 1.
+
+For consistency you need to make sure that all log messages are passed
+through the same filters in the same order in all the templates that
+display log messages.
+
+=item 2.
+
+If you move the templates from a machine that has a particular filter
+installed to a machine that doesn't have that filter installed you need
+to remove it from the template, otherwise you will receive a run-time
+error.
+
+=back
+
+SVN::Web provides a special Template::Toolkit filter called C<log_msg>.
+Use it like so (assume C<msg> contains the SVN log message).
+
+  [% msg | log_msg %]
+
+The filters to run for C<log_msg>, their order, and any options, are
+specified in the C<log_msg_filters> configuration directive.  This contains
+a list of filters and key,value pairs.
+
+=over 4
+
+=item name
+
+Specifies the name of the class of the filtering object or plugin.  If
+the object is in the Template::Plugin::* namespace then you can omit
+the leading C<Template::Plugin::>.
+
+If the name is C<standard> then filters from the standard
+L<Template::Filters> collection can be used.
+
+=item filter
+
+The name of the filter to run.  This is taken from whatever the plugin's
+documentation says would go in the C<[% FILTER ... %]> directive.
+
+For example, the L<Template::Plugin::Clickable> documentation gives this
+example;
+
+  [% USE Clickable %]
+  [% FILTER clickable %]
+  ...
+
+So the correct value for C<name> is C<Clickable>, and the correct value for
+C<filter> is C<clickable>.
+
+=item opts
+
+Any options can be passed to the filter using C<opts>.  This specifies a
+list of hash key,value pairs.
+
+  ...
+  opts:
+    - first_opt: first_value
+      second_opt: second_value
+  ...
+
+=back
+
+Filters are run in the order they are listed.  Any filters that do not exist
+on the system are ignored.
+
+The configuration file includes a suggested list of default filters.
+
+=head2 Actions and action classes
+
+Each action that SVN::Web can carry out is implemented as a class (see
+L</"ACTIONS, SUBCLASSES, AND URLS"> for more).  You can specify your own
+class for a particular action.  This lets you implement your own actions,
+or override the behaviour of existing actions.
+
+The complete list of actions is listed in the C<actions> configuration
+directive.  The default value for this directive if it is not present is;
+
+  actions:
+    - browse
+    - checkout
+    - diff
+    - list
+    - log
+    - revision
+    - RSS
+    - view
+
+If you delete items from this list then the corresponding action becomes
+unavailable.  For example, if you would like to prevent people from retrieving
+an RSS feed of changes, just delete the C<- RSS> entry from the list.
+
+To provide your own behaviour for standard actions, specify an C<<
+<action>_class >> configuration that names the class that implements
+the action.  For example, to specify your own class that implements
+the C<view> action;
+
+  view_class: My::SVN::Web::View
+
+If you wish to implement your own action, give the action a name, add
+it to the C<actions> list, and then specify the class that carries out
+the action.
+
+For example, SVN::Web currently provides no equivalent to the
+Subversion C<annotate> command.  If you implement this, you would write:
+
+  actions:
+    - ...
+    - annotate
+    - ...
+
+  annotate_class: My::Class::That::Implements::Annotate
+
+Naturally, you would submit this back to the maintainers so that it can
+be included in the standard distribution.
+
+=head2 CGI class
+
+SVN::Web can use a custom CGI class.  By default SVN::Web will use CGI::Fast
+if it is installed, and fallback to using CGI otherwise.
+
+If you have your own CGI subclass you can specify it here.
+
+   cgi_class: 'My::CGI::Subclass'
+
+This option is somewhat specialised.
+
+=head1 ACTIONS, SUBCLASSES, AND URLS
+
+SVN::Web URLs are broken down in to four components.
+
+  .../index.cgi/<repo>/<action>/<path>?<arguments>
+
+=over 4
+
+=item I<repo>
+
+The repository the action will be performed on.  SVN::Web can be
+configured to operate on multiple Subversion repositories.
+
+=item I<action>
+
+The action that will be run.
+
+=item I<path>
+
+The path within the <repository> that the action is performed on.
+
+=item I<arguments>
+
+Any arguments that control the behaviour of the I<action>.
+
+=back
+
+Each action is implemented as a Perl module.  By convention, each module
+carries out whatever processing is required by the action, and returns a
+reference to a hash of data that is used to fill out a C<Template::Toolkit>
+template that displays the action's results.
+
+The standard actions, and the Perl modules that implement them, are:
+
+=over 4
+
+=item I<browse>, I<SVN::Web::Browse>
+
+Shows the files and directories in a given repository path.  This is
+the default command if no path is specified in the URL.
+
+=item I<checkout>, I<SVN::Web::Checkout>
+
+Returns the raw data for the file at a given repository path and revision.
+
+=item I<diff>, I<SVN::Web::Diff>
+
+Shows the difference between two revisions of the same file.
+
+=item I<list>, I<SVN::Web::List>
+
+Lists the available Subversion repositories.  This is the default
+command if no repository is specified in the URL.
+
+=item I<log>, I<SVN::Web::Log>
+
+Shows log information (commit messages) for a given repository path.
+
+=item I<revision>, I<SVN::Web::Revision>
+
+Shows information about a specific repository revision.
+
+=item I<rss>, I<SVN::Web::RSS>
+
+TODO
+
+=item I<view>, I<SVN::Web::View>
+
+Shows the commit message and file contents for a specific repository path
+and revision.
+
+=back
+
+See the documentation for each of these modules for more information
+about the data that they provide to each template, and for information
+about customising the templates used for each module.
+
+=over 4
+
 =head1 MOD_PERL
 
 You can enable mod_perl support of SVN::Web with the following in the
@@ -63,10 +337,6 @@ apache configuration:
 
 =head1 BUGS
 
-Note that the first time for accessing a repository might be very
-slow, because the Branch plugin has to create cache for copy
-information. for a large 9000-revision repository it takes 2 minutes.
-
 =cut
 
 my $template;
@@ -74,7 +344,8 @@ my $config;
 
 my %REPOS;
 
-our @PLUGINS = qw/branch browse checkout diff list log revision RSS template view/;
+our @DEFAULT_ACTIONS
+  = qw(browse checkout diff list log revision RSS view);
 
 sub load_config {
     my $file = shift || 'config.yaml';
@@ -143,7 +414,6 @@ sub get_handler {
     return $pkg->new (%$cfg, reposname => $cfg->{repos},
 		      repos => $repos,
                       config => $config);
-
 }
 
 sub run {
@@ -233,7 +503,8 @@ sub get_template {
     Template->new ({ INCLUDE_PATH => ($config->{templatedir} || 'template/default/'),
 		     PRE_PROCESS => 'header',
 		     POST_PROCESS => 'footer',
-		     FILTERS => { l => ([\&loc_filter, 1]) } });
+		     FILTERS => { l => ([\&loc_filter, 1]),
+				  log_msg => \&log_msg_filter, } });
 }
 
 sub run_cgi {
@@ -242,12 +513,17 @@ sub run_cgi {
     load_config ('config.yaml');
     $template ||= get_template ();
 
+    $config->{actions} ||= \@DEFAULT_ACTIONS;
+
     my $cgi_class = $config->{cgi_class} || (eval { require CGI::Fast; 1 } ? 'CGI::Fast' : 'CGI');
     while (my $cgi = $cgi_class->new) {
 	# /<repository>/<action>/<path>/<file>?others
 	my (undef, $repos, $action, $path) = split ('/', $cgi->path_info, 4);
 	$action ||= 'browse';
 	$path ||= '';
+
+	die "action '$action' not supported" 
+	  unless scalar grep(/^$action$/, @{$config->{actions}});
 
 	run ({ repos => $repos,
 	       action => $action,
@@ -264,6 +540,42 @@ sub loc_filter {
     my $context = shift;
     my @args = @_;
     return sub { loc($_[0], @args) };
+}
+
+# A meta-filter for log messages.  Processes a list of hashes.  Each hash
+# has (at least) 'name' and 'filter' keys.  The 'name' is the Perl module
+# that implements the plugin.  The 'filter' key is the name of the plugin's
+# method that does the filtering.
+#
+# The optional 'opts' key is a list of option key/value pairs to pass
+# to the filter.
+sub log_msg_filter {
+  my $text = shift;
+
+  my $plugin;
+  my @filters = @{ $config->{log_msg_filters} };
+  my $context = $template->context();
+
+  foreach my $filter_spec (@filters) {
+    if($filter_spec->{name} ne 'standard') {
+      eval {			# Make sure plugin is available, skip if not
+	$plugin = $context->plugin($filter_spec->{name});
+      };
+      if($@) {
+	warn "Plugin $filter_spec->{name} is not available\n";
+	next;
+      }
+    }
+
+    if(defined $plugin and $plugin->can('filter')) {
+      $text = $plugin->filter($text);
+    } else {
+      my $filter = $context->filter($filter_spec->{filter},
+				    $filter_spec->{opts}) || next;
+      $text = $filter->($text);
+    }
+  }
+  return $text;
 }
 
 sub handler {
@@ -311,11 +623,15 @@ sub handler {
 
 =head1 AUTHORS
 
-Chia-liang Kao E<lt>clkao@clkao.orgE<gt>
+Chia-liang Kao C<< <clkao@clkao.org> >>
+
+Nik Clayton C<< <nik@FreeBSD.org> >>
 
 =head1 COPYRIGHT
 
-Copyright 2003-2004 by Chia-liang Kao E<lt>clkao@clkao.orgE<gt>.
+Copyright 2003-2004 by Chia-liang Kao C<< <clkao@clkao.org> >>.
+
+Copyright 2005 by Nik Clayton C<< <nik@FreeBSD.org> >>.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
