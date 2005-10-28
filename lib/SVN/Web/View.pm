84f76bed27b47970e679a22540235a69c2d6c8ec
nik <nik@714b6732-7f46-11dd-bf14-85d1fe059cc0>
2005-10-28 15:26:04 +0000

    Start the documentation for this module.

--- lib/SVN/Web/View.pm
+++ lib/SVN/Web/View.pm
@@ -4,6 +4,79 @@ use SVN::Core;
 use SVN::Repos;
 use SVN::Fs;
 
+=head1 NAME
+
+SVN::Web::View - SVN::Web action to view a file in the repository
+
+=head1 SYNOPSIS
+
+In F<config.yaml>
+
+  actions:
+    ...
+    - view
+    ...
+
+  view_class: SVN::Web::View
+
+=head1 DESCRIPTION
+
+Shows a specific revision of a file in the Subversion repository.  Includes
+the commit information for that file.
+
+=head1 OPTIONS
+
+=over 8
+
+=item rev
+
+The revision of the file to show.  Defaults to the repository's
+youngest revision.
+
+If this is not an interesting revision for this file, the repository history
+is searched to find the youngest interesting revision for this file that is
+less than C<rev>.
+
+=back
+
+=head1 TEMPLATE VARIABLES
+
+=over 8
+
+=item rev
+
+The revision that has been returned.  This is not necessarily the same
+as the C<rev> option passed to the action.
+
+=item mimetype
+
+The file's MIME type, extracted from the file's C<svn:mime-type>
+property.  If this is not set then C<text/plain> is used.
+
+=item file
+
+The contents of the file.
+
+=item author
+
+The revision's author.
+
+=item date
+
+The date the revision was committed.
+
+=item msg
+
+The revision's commit message.
+
+=back
+
+=head1 EXCEPTIONS
+
+None.
+
+=cut
+
 sub new {
     my $class = shift;
     my $self = bless {}, $class;
