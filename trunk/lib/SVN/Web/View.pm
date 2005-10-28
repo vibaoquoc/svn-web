ebc60442eb3629990c7e1924fa152ee5ebff3c9c
nik <nik@714b6732-7f46-11dd-bf14-85d1fe059cc0>
2005-10-28 15:17:00 +0000

    Add a new command, 'view', with associated module and template.
    This command takes a repository path and an (optional) revision.  If
    the revision is not specified then it defaults to the repository's
    youngest revision.
    
    It searches the history to find the youngest interesting revision from
    the one given.  It then displayes the log message for that revision, 
    and the contents of the file at that revision.
    
    The effect is similar to Trac's default behaviour for viewing a file.
    For a Trac  example, see 
    
     http://jc.ngo.org.uk/trac-bin/trac.cgi/file/nik/libtap/trunk/src/tap.3?rev=353

---
+++ trunk/lib/SVN/Web/View.pm
@@ -0,0 +1,59 @@
+package SVN::Web::View;
+use strict;
+use SVN::Core;
+use SVN::Repos;
+use SVN::Fs;
+
+sub new {
+    my $class = shift;
+    my $self = bless {}, $class;
+    %$self = @_;
+
+    return $self;
+}
+
+sub _log {
+    my ($self, $paths, $rev, $author, $date, $msg, $pool) = @_;
+#    my ($self, $rev, $root, $paths, $props) = @_;
+    return unless $rev > 0;
+#    my ($author, $date, $message) = @{$props}{qw/svn:author svn:date svn:log/};
+
+    my $data = { rev => $rev, author => $author,
+		 date => $date, msg => $msg };
+    $data->{paths} = { map { $_ => { action => $paths->{$_}->action(),
+				     copyfrom => $paths->{$_}->copyfrom_path(),
+				     copyfromrev => $paths->{$_}->copyfrom_rev(),
+				     }} keys %$paths};
+
+    return $data;
+}
+
+sub run {
+    my $self = shift;
+    my $pool = SVN::Pool->new_default_sub;
+    my $fs = $self->{repos}->fs;
+    my $rev = $self->{cgi}->param('rev') || $fs->youngest_rev;
+    my $root = $fs->revision_root($rev);
+
+    # Start at $rev, and look backwards for the first interesting
+    # revision number for this file.
+    my $hist = $root->node_history($self->{path});
+    $hist = $hist->prev(0);
+    $rev = ($hist->location())[1];
+
+    # Get the log for this revision of the file
+    $self->{repos}->get_logs([$self->{path}], $rev - 1, $rev, 1, 0,
+                             sub { $self->{REV} = $self->_log(@_)});
+
+    # Get the text for this revision of the file
+    $root = $fs->revision_root($rev);
+    my $file = $root->file_contents($self->{path});
+    local $/;
+    return {template => 'view',
+	    data => { rev => $rev,
+		      file => <$file>,
+		      %{$self->{REV}},
+		    }};
+}
+
+1;
