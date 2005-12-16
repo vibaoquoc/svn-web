d1e4e896724066763d5b4b970309a7f7f97b2ced
nik <nik@714b6732-7f46-11dd-bf14-85d1fe059cc0>
2005-12-16 12:20:37 +0000

    If there's only one repo listed in the config file then the 'list' action jumps straight to the 'browse' action for that repo, saving the user an intermediate step.

--- lib/SVN/Web/List.pm
+++ lib/SVN/Web/List.pm
@@ -14,6 +14,16 @@ sub run {
     my $self = shift;
 
     my @repos = SVN::Web::repos_list();
+
+    # If there's only one repo listed then jump straight to it
+    if(@repos == 1) {
+        my $url = $self->{cgi}->self_url();
+	$url =~ s{/$}{};
+	$url .= "/$repos[0]";
+        print $self->{cgi}->redirect(-uri => $url);
+	return;
+    }
+
     return { template => 'list',
 	     data => {
 		      action => 'list',
